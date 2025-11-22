de31a1d33eaa25f0c7187ab040344471baeecceb
Ghislain1 <ghislain1@outlook.de>
2025-11-22 15:37:55 +0000

    Update docs build

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -14,7 +14,7 @@ export default {
   assets: {
     'index.csr.html': {size: 980, hash: 'c37149128cfbf8d6e7f1f5f55999261a549d2ab05d92913514a87cf506961bb3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 1387, hash: '1d27279012866301c7ddec975a381ba24b684ce2dea84cf70a4e767a2ef70418', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 24247, hash: '710d8d7e1d4f849c41a47b0ffd59773959a8a0347396bb232bd568e4905e224e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 24259, hash: '4043ed7bc8be18c7c3005f7ae8721072ccea45b876e1de051a0bf58fdf0691bc', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-TEC6ZZIG.css': {size: 142, hash: 'oc2kvQHchC4', text: () => import('./assets-chunks/styles-TEC6ZZIG_css.mjs').then(m => m.default)}
   },
 };
